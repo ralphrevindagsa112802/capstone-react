4f3122360b1071d31a0d2db28d0ea618eb0881e6
Mark Angelo Gerodiaz <gerodiazmarkangelo02@gmail.com>
2025-02-18 17:00:33 +0800

    fetch user profile

---
+++ api/fetch_user.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
+$servername = "localhost"; // Adjust if needed
+$username = "root"; // Your database username
+$password = ""; // Your database password
+$dbname = "yappari"; // Replace with your actual database name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["success" => false, "error" => "Database connection failed"]));
+}
+
+if (isset($_SESSION["user_id"])) {
+    $user_id = $_SESSION["user_id"];
+    
+    $query = "SELECT username, f_name, l_name, email, phone, address FROM users WHERE ID = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($row = $result->fetch_assoc()) {
+        echo json_encode(["success" => true, "user" => $row]);
+    } else {
+        echo json_encode(["success" => false, "error" => "User not found"]);
+    }
+
+    $stmt->close();
+} else {
+    echo json_encode(["success" => false, "error" => "User not logged in"]);
+}
+
+$conn->close();
+?>
--- src/pages/useraccount/UserAccount.jsx
+++ src/pages/useraccount/UserAccount.jsx
@@ -1,154 +1,177 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserNavbar from '../../components/UserNavbar';
 import Footer from '../../components/Footer';
 
 const UserAccount = () => {
-  // ✅ State to enable/disable form fields
   const [isEditing, setIsEditing] = useState(false);
+  const [userData, setUserData] = useState({
+    username: '',
+    f_name: '',
+    l_name: '',
+    email: '',
+    phone: '',
+    address: '',
+  });
 
-  // ✅ Function to toggle edit mode
-  const handleEditClick = () => {
-    setIsEditing(!isEditing);
-  };
+  useEffect(() => {
+    fetch('http://localhost/capstone-react/api/fetch_user.php', { // Adjust API URL if needed
+      credentials: 'include',
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          setUserData(data.user);
+        } else {
+          console.error(data.error);
+        }
+      })
+      .catch(error => console.error('Error fetching user data:', error));
+  }, []);
 
   return (
     <div>
       <UserNavbar />
-
+      
       <div className="flex flex-row bg-[#1C359A] py-10 px-4 md:px-36 mt-32 ">
         <div className="flex flex-row absolute -mb-42 md:flex-row">
           <div className="w-40 h-40 shadow-2xl rounded-full bg-white flex items-center justify-center text-[#1C359A] text-2xl md:text-4xl">
-            <span id="userInitials">--</span>
+            <span id="userInitials">{userData.f_name.charAt(0) + userData.l_name.charAt(0)}</span>
           </div>
           <div className="mt-8 md:mt-8 md:ml-4 text-center md:text-left">
-            <h2 className="text-lg md:text-xl text-white font-semibold" id="userName">Loading...</h2>
-            <p className="text-sm text-white" id="userAddress">Loading...</p>
+            <h2 className="text-lg md:text-xl text-white font-semibold" id="userName">
+              {userData.f_name} {userData.l_name}
+            </h2>
+            <p className="text-sm text-white" id="userAddress">{userData.address}</p>
           </div>
         </div>
 
-        {/* ✅ Toggle edit mode on click */}
         <button 
           id="editProfileBtn" 
           className="md:mt-8 mt-8 md:ml-auto px-4 py-2 bg-white text-black font-bold rounded-md"
-          onClick={handleEditClick}
+          onClick={() => setIsEditing(!isEditing)}
         >
           {isEditing ? "Cancel" : "Edit Profile"}
         </button>
       </div>
 
       <div className="container mx-auto pt-6 px-4 md:px-36 flex flex-col md:flex-row w-full">
-        <aside className="w-full mt-12 md:w-64 h-auto md:h-screen py-4 flex flex-col space-y-6">
-          <nav className="space-y-4">
-            <a id="profile" href="profile.html" className="flex items-center space-x-4">
-              <img src="path-to-user-profile-icon.svg" alt="User Profile" className="w-6 h-6"/>
-              <span className="font-semibold">User profile</span>
-            </a>
-            <a id="cart" href="cart.html" className="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
-              <img src="path-to-cart-icon.svg" alt="Cart" className="w-6 h-6"/>
-              <span className="font-semibold">Cart</span>
-            </a>
-            <a id="orderstatus" href="orderstatus.html" className="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
-              <img src="path-to-order-status-icon.svg" alt="Order Status" className="w-6 h-6"/>
-              <span className="font-semibold">Order status</span>
-            </a>
-            <a id="orderhistory" href="orderhistory.html" className="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
-              <img src="path-to-order-history-icon.svg" alt="Order History" className="w-6 h-6"/>
-              <span className="font-semibold">Order History</span>
-            </a>
-          </nav>
-          <div className="mt-6">
-            <button className="flex items-center justify-center w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
-              <img src="path-to-sign-out-icon.svg" alt="Sign Out" className="w-5 h-5 mr-2"/>
-              SIGN OUT
-            </button>
-          </div>
-        </aside>
+      {/**side bar profile section */}
+      <aside class="w-full mt-12 md:w-64 h-auto md:h-screen py-4 flex flex-col space-y-6">
+                    <nav class="space-y-4">
+                
+                        <a id="profile" href="UserAccount.jsx" class="flex items-center space-x-4">
+                        <img src="path-to-user-profile-icon.svg" alt="User Profile" class="w-6 h-6"/>
+                        <span class="font-semibold">User profile</span>
+                        </a>
 
-      <div className="container mx-auto pt-6 px-4 md:px-36 flex flex-col md:flex-row w-full">
+                    
+                        <a id="cart" href="cart.html" class="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
+                        <img src="path-to-cart-icon.svg" alt="Cart" class="w-6 h-6"/>
+                        <span class="font-semibold">Cart</span>
+                        </a>
+
+                    
+                        <a id="orderstatus" href="orderstatus.html" class="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
+                        <img src="path-to-order-status-icon.svg" alt="Order Status" class="w-6 h-6"/>
+                        <span class="font-semibold">Order status</span>
+                        </a>
+
+                    
+                        <a id="orderhistory" href="orderhistory.html" class="flex items-center space-x-4 text-gray-800 hover:text-blue-600">
+                        <img src="path-to-order-history-icon.svg" alt="Order History" class="w-6 h-6"/>
+                        <span class="font-semibold">Order History</span>
+                        </a>
+                    </nav>
+
+                    
+                    <div class="mt-6">
+                        <button class="flex items-center justify-center w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
+                        <img src="path-to-sign-out-icon.svg" alt="Sign Out" class="w-5 h-5 mr-2"/>
+                        SIGN OUT
+                        </button>
+                    </div>
+                    </aside>
+
+      {/** main content*/}
+
         <div className="flex-grow ml-0 md:ml-6 p-4">
-          <div className="h-full">
-            <form id="profileForm" className="flex flex-col h-full space-y-4">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <div>
-                  <input 
-                    type="text" 
-                    id="firstName" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
-                    placeholder="First Name" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-                <div>
-                  <input 
-                    type="text" 
-                    id="lastName" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`}  
-                    placeholder="Last Name" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <div>
-                  <input 
-                    type="text" 
-                    id="userNameInput" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
-                    placeholder="User Name" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-                <div>
-                  <input 
-                    type="email" 
-                    id="email" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
-                    placeholder="Email Address" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <div>
-                  <input 
-                    type="text" 
-                    id="location" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
-                    placeholder="Location" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-                <div>
-                  <input 
-                    type="text" 
-                    id="phone" 
-                    className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
-                    placeholder="Phone Number" 
-                    disabled={!isEditing} 
-                  />
-                </div>
-              </div>
-              <div className="flex justify-end">
-                {isEditing && (
-                  <button 
-                    type="button" 
-                    id="saveProfileBtn" 
-                    className="px-4 py-2 bg-green-500 text-white rounded-md"
-                    onClick={() => setIsEditing(false)}
-                  >
-                    Save Changes
-                  </button>
-                )}
-              </div>
-            </form>
-          </div>
+          <form id="profileForm" className="flex flex-col h-full space-y-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+              <input 
+                type="text" 
+                id="firstName" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
+                placeholder="First Name"
+                value={userData.f_name} 
+                disabled={!isEditing} 
+              />
+              <input 
+                type="text" 
+                id="lastName" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`}  
+                placeholder="Last Name" 
+                value={userData.l_name}
+                disabled={!isEditing} 
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+              <input 
+                type="text" 
+                id="userNameInput" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
+                placeholder="User Name"
+                value={userData.username} 
+                disabled={!isEditing} 
+              />
+              <input 
+                type="email" 
+                id="email" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
+                placeholder="Email Address"
+                value={userData.email} 
+                disabled={!isEditing} 
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+              <input 
+                type="text" 
+                id="location" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
+                placeholder="Address"
+                value={userData.address} 
+                disabled={!isEditing} 
+              />
+              <input 
+                type="text" 
+                id="phone" 
+                className={`w-full mt-1 p-2 rounded-md ${isEditing ? 'border' : 'border-none'}`} 
+                placeholder="Phone Number"
+                value={userData.phone} 
+                disabled={!isEditing} 
+              />
+            </div>
+
+            <div className="flex justify-end">
+              {isEditing && (
+                <button 
+                  type="button" 
+                  id="saveProfileBtn" 
+                  className="px-4 py-2 bg-green-500 text-white rounded-md"
+                  onClick={() => setIsEditing(false)}
+                >
+                  Save Changes
+                </button>
+              )}
+            </div>
+          </form>
         </div>
-      </div>
       </div>
 
       <Footer />
     </div>
   );
-}
+};
 
 export default UserAccount;
