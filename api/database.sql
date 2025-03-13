db9f7af3fe3cc6f0a7a7a646796fab0c4383341c
Ralph Dagsa <ralphrevindagsa112802@gmail.com>
2025-03-13 21:39:15 +0800

    Update database.sql

--- api/database.sql
+++ api/database.sql
@@ -64,6 +64,7 @@ CREATE TABLE food (
     availability_large VARCHAR(255) DEFAULT "Not Available",
     image_path VARCHAR(255) DEFAULT NULL,
     description TEXT NOT NULL,
+    allergen TEXT NOT NULL,
     INDEX (category)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
